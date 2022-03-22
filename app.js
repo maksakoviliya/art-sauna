e91dd78ac8a972408fb237415e42a62020a035be
maksak_il <maksak_il@mail.ru>
2022-03-22 16:57:35 +0300

    Rebuild with images

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
 var __webpack_exports__ = {};
 /*!********************!*\
   !*** ./src/app.js ***!
   \********************/
-__webpack_require__.r(__webpack_exports__);
 new Accordion('.accordion-container-first', {
   duration: 75,
   openOnInit: [0]
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,9 @@
 let mix = require('laravel-mix');
+require('laravel-mix-simple-image-processing')
 
-mix.js('src/app.js', '/').setPublicPath('/');
+mix.js('src/app.js', '/')
+    .imgs({
+        source: 'src/img',
+        destination: 'img',
+    })
+    .setPublicPath('/');
