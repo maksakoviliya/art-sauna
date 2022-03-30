a0e1e616a69197e706b81f59872e3725f04f472b
maksak_il <maksak_il@mail.ru>
2022-03-30 15:00:24 +0300

    Some fixs

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,4 @@
 let mix = require('laravel-mix');
-require('laravel-mix-simple-image-processing')
 
 mix.js('src/app.js', '/')
-    .imgs({
-        source: 'src/img',
-        destination: 'img',
-    })
     .setPublicPath('/');
