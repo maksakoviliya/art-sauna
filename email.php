6fcf8ca4fb5cf20a85c37769b27e7edae3ca8f0e
maksak_il <maksak_il@mail.ru>
2022-04-05 00:35:31 +0300

    Update email.php

--- email.php
+++ email.php
@@ -1,14 +1,11 @@
 <?php
-//if (!empty($_POST['name'])) {
+if (!empty($_POST['name'])) {
     $to = 'maksak_il@mail.ru';
     $subject = 'the subject';
-    $message = 'hello' . $_POST['name'] . 'ddd';
+    $message = 'hello';
     $headers = 'From: no-reply@art-sauna.com' . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
 
     mail($to, $subject, $message, $headers);
     echo 'success';
-//} else {
-//    echo 'no data';
-//}
-?>
+}
