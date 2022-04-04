7f4fc86a9dd649267614da694879caa2f49a41d8
maksak_il <maksak_il@mail.ru>
2022-04-05 00:34:08 +0300

    Temp

--- app.js
+++ app.js
@@ -250,15 +250,7 @@ $(document).ready(function () {
     e.preventDefault();
     $.ajax({
       type: "POST",
-      url: "email.php",
-      contentType: 'application/json',
-      dataType: 'json',
-      data: JSON.stringify({
-        name: 'no-reply@art-sauna.ru',
-        email: 'no-reply@art-sauna.ru',
-        subject: 'subject',
-        message: 'I am message'
-      })
+      url: "email.php"
     }).done(function (data) {
       console.log('data', data);
     });
--- src/app.js
+++ src/app.js
@@ -271,14 +271,6 @@ $(document).ready(function () {
         $.ajax({
             type: "POST",
             url: "email.php",
-            contentType: 'application/json',
-            dataType: 'json',
-            data: JSON.stringify({
-                name: 'no-reply@art-sauna.ru',
-                email: 'no-reply@art-sauna.ru',
-                subject: 'subject',
-                message: 'I am message',
-            }),
         }).done(function(data) {
             console.log('data', data)
         });
