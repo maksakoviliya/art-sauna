3d0fa7da74e8edd1a2ee0921a27c5e36a3f565cb
maksak_il <maksak_il@mail.ru>
2022-04-05 00:24:39 +0300

    Test

--- src/app.js
+++ src/app.js
@@ -271,6 +271,8 @@ $(document).ready(function () {
         $.ajax({
             type: "POST",
             url: "email.php",
+            contentType: 'application/json',
+            dataType: 'json',
             data: JSON.stringify({
                 name: 'no-reply@art-sauna.ru',
                 email: 'no-reply@art-sauna.ru',
