1559bdbc3ade3e30671184ac7b603ff1f0dbdf81
maksak_il <maksak_il@mail.ru>
2022-04-05 00:47:24 +0300

    Update email.php

--- email.php
+++ email.php
@@ -4,8 +4,8 @@ $subject = 'the subject';
 $message = 'hello';
 $headers = 'From: no-reply@art-sauna.com'       . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
-$json = file_get_contents('php://input');
-$data = json_decode($json);
-print_r($data);
+//$json = file_get_contents('php://input');
+//$data = json_decode($json);
+//print_r($data);
 //mail($to, $subject, $message, $headers);
 echo 'success';
