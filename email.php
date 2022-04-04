dcb80f85b84b7aedacd2676aa43f7e3bd7c9201c
maksak_il <maksak_il@mail.ru>
2022-04-05 00:48:35 +0300

    Update email.php

--- email.php
+++ email.php
@@ -1,11 +1,16 @@
 <?php
-$to      = 'maksak_il@mail.ru';
-$subject = 'the subject';
-$message = 'hello';
-$headers = 'From: no-reply@art-sauna.com'       . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-//$json = file_get_contents('php://input');
-//$data = json_decode($json);
-//print_r($data);
+//$to      = 'maksak_il@mail.ru';
+//$subject = 'the subject';
+//$message = 'hello';
+//$headers = 'From: no-reply@art-sauna.com'       . "\r\n" .
+//    'X-Mailer: PHP/' . phpversion();
+$json = file_get_contents('php://input');
+
+// decode the json data
+echo $_POST['name'];
+$data = json_decode($json);
+echo 'data \n /n /r \r';
+print_r($data);
+echo 'here';
 //mail($to, $subject, $message, $headers);
 echo 'success';
