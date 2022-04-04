93c9d04598cf9821326a849d821e3d25c13c1415
maksak_il <maksak_il@mail.ru>
2022-04-05 00:17:29 +0300

    Update email.php

--- email.php
+++ email.php
@@ -1,35 +1,10 @@
 <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
+$to      = 'maksak_il@mail.ru';
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: no-reply@art-sauna.com'       . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
-require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
-
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
-
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'ssl://smtp.mail.ru';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'artsaunaleads@mail.ru';                     //SMTP username
-    $mail->Password   = 'wsf#%gts44';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-    //Recipients
-    $mail->setFrom('no-reply@art-sauna.com', 'Mailer');
-    $mail->addAddress('maksak_il@mail.ru');               //Name is optional
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+mail($to, $subject, $message, $headers);
+echo 'success'
+?>
