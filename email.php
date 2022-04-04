46d688c31a05012db2e801a0b1768a91df771f09
maksak_il <maksak_il@mail.ru>
2022-04-04 23:49:49 +0300

    Update email.php

--- email.php
+++ email.php
@@ -10,7 +10,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 //require 'phpmailer/PHPMailer.php';
-require './vendor/phpmailer/PHPMailerAutoload.php';
+require './vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
 
 if (!empty($_POST['name'])) {
 
