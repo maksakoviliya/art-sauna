017722bba6c7c3e8673a9743d5bf0af8ef525034
maksak_il <maksak_il@mail.ru>
2022-04-05 00:44:20 +0300

    Update email.php

--- email.php
+++ email.php
@@ -7,8 +7,9 @@ $headers = 'From: no-reply@art-sauna.com'       . "\r\n" .
 $json = file_get_contents('php://input');
 
 // decode the json data
+echo $_POST['name'];
 $data = json_decode($json);
-echo 'data';
+echo 'data \n /n /r \r';
 print_r($data);
 echo 'here';
 //mail($to, $subject, $message, $headers);
