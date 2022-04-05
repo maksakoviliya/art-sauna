a66013c001d5067f09df5dc8f72dca22cef6e511
maksak_il <maksak_il@mail.ru>
2022-04-05 09:25:21 +0300

    Update email.php

--- email.php
+++ email.php
@@ -1,7 +1,7 @@
 <?php
-$to      = 'maksak_il@mail.ru';
+$to = '20785813.293103@parser.amocrm.ru';
 $subject = 'the subject';
-$headers = 'From: no-reply@art-sauna.com'       . "\r\n" .
+$headers = 'From: no-reply@art-sauna.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 $json = file_get_contents('php://input');
@@ -10,6 +10,13 @@ $data = json_decode($json);
 $roistatVisitId = array_key_exists('roistat_visit', $_COOKIE) ? $_COOKIE['roistat_visit'] : 'nocookie';
 
 $message = "Телефон: $data->phone; Название формы: Форма; Roistat: {$roistatVisitId};";
+if (isset($data->name)) {
+    $message = $message . "Имя: $data->name";
+}
+
+if (isset($data->text)) {
+    $message = $message . "Текст: $data->text";
+}
 
 mail($to, $subject, $message, $headers);
 echo $message;
